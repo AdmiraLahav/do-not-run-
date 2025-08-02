d91954b5a50083798a16bc7adc5b99626c1aa3f0
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-08-02 14:50:11 +0300

    Update ZipBombzipbomb.py

--- credit:abdulfatir/ZipBombzipbomb.py
+++ credit:abdulfatir/ZipBombzipbomb.py
@@ -46,15 +46,21 @@ if __name__ == '__main__':
 	level_1_zip = '1.zip'
 	compress_file(dummy_name, level_1_zip)
 	os.remove(dummy_name)
-	decompressed_size = 1
-	for i in range(1,n_levels+1):
-		make_copies_and_compress('%d.zip'%i,'%d.zip'%(i+1),10)
-		decompressed_size *= 10
-		os.remove('%d.zip'%i)
+	decompressed_size_mb = 1  # base dummy size = 1MB
+	for i in range(1, n_levels + 1):
+	    make_copies_and_compress('%d.zip' % i, '%d.zip' % (i + 1), 10)
+	    decompressed_size_mb *= 10
+	    os.remove('%d.zip' % i)
+	
 	if os.path.isfile(out_zip_file):
-		os.remove(out_zip_file)
-	os.rename('%d.zip'%(n_levels+1),out_zip_file)
+	    os.remove(out_zip_file)
+	os.rename('%d.zip' % (n_levels + 1), out_zip_file)
+	
 	end_time = time.time()
-	print ('Compressed File Size: %.2f KB'%(get_file_size(out_zip_file)/1024.0))
-	print ('Size After Decompression: %d GB'%decompressed_size)
-	print ('Generation Time: %.2fs'%(end_time - start_time))
+	compressed_kb = get_file_size(out_zip_file) / 1024.0
+	decompressed_gb = decompressed_size_mb / 1024
+	
+	print('Compressed File Size: %.2f KB' % compressed_kb)
+	print('Estimated Size After Decompression: %.2f GB' % decompressed_gb)
+	print('Generation Time: %.2fs' % (end_time - start_time))
+	
